2fbf0cda6bc0c70a36bdf2a2a6e884e3b9761be0
Pawan Gowda <38250665+pa1kumarbh@users.noreply.github.com>
2020-02-18 13:46:39 +0530

    Update Program.cs
    reverted changes

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace k8s_cicd   # this is a comment 
+namespace k8s_cicd   
 {
     public class Program
     {
